6e6301c628a2431a072daed14b76e86bb0297cae
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-10-12 00:54:29 +0700

    feat:project completed

--- src/data/product.js
+++ src/data/product.js
@@ -2,35 +2,35 @@ const products = [
   {
     id: 1,
     name: "Fond - Neutral",
-    price: 160,
+    price: 100,
     image: "http://dummyimage.com/350x350.png/dddddd/000000",
     description: "Morbi non quam nec dui luctus rutrum. Nulla tellus.",
   },
   {
     id: 2,
     name: "Pepper - Cubanelle",
-    price: 7624,
+    price: 200,
     image: "http://dummyimage.com/350x350.png/cc0000/ffffff",
     description: "Nulla facilisi.",
   },
   {
     id: 3,
     name: "Cleaner - Pine Sol",
-    price: 7457,
+    price: 300,
     image: "http://dummyimage.com/350x350.png/cc0000/ffffff",
     description: "Maecenas rhoncus aliquam lacus.",
   },
   {
     id: 4,
     name: "Wakami Seaweed",
-    price: 4824,
+    price: 400,
     image: "http://dummyimage.com/350x350.png/ff4444/ffffff",
     description: "In hac habitasse platea dictumst.",
   },
   {
     id: 5,
     name: "Dried Figs",
-    price: 1112,
+    price: 500,
     image: "http://dummyimage.com/350x350.png/5fa2dd/ffffff",
     description:
       "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
@@ -38,14 +38,14 @@ const products = [
   {
     id: 6,
     name: "Kohlrabi",
-    price: 4649,
+    price: 600,
     image: "http://dummyimage.com/350x350.png/5fa2dd/ffffff",
     description: "Praesent blandit.",
   },
   {
     id: 7,
     name: "Kippers - Smoked",
-    price: 3633,
+    price: 700,
     image: "http://dummyimage.com/350x350.png/dddddd/000000",
     description:
       "Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam. Nam tristique tortor eu pede.",
@@ -53,7 +53,7 @@ const products = [
   {
     id: 8,
     name: "Wine - White, Ej Gallo",
-    price: 8004,
+    price: 800,
     image: "http://dummyimage.com/350x350.png/ff4444/ffffff",
     description:
       "Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
@@ -61,14 +61,14 @@ const products = [
   {
     id: 9,
     name: "The Pop Shoppe - Grape",
-    price: 2646,
+    price: 900,
     image: "http://dummyimage.com/350x350.png/ff4444/ffffff",
     description: "Nulla mollis molestie lorem. Quisque ut erat.",
   },
   {
     id: 10,
     name: "Currants",
-    price: 9419,
+    price: 1000,
     image: "http://dummyimage.com/350x350.png/5fa2dd/ffffff",
     description:
       "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
